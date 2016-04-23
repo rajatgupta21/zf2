a0af5c7b937488a7bc2e589257662b651175550c
rajat <rajat@localhost.localdomain>
2016-04-23 11:45:09 +0530

    Added sql file and changed ReadMe file to add procedure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # zf2
+# Here we have htdocs_content folder copy this folder in htdoc and then rename from htdocs_content to anyother project name like test
+# then just browse http://localhost/test/public/blog OR http://localhost/test/public/album
+# do not forget to import and configure DB using .sql file for table blog and Album 
---
+++ zf2tutorial.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 23, 2016 at 08:11 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `zf2tutorial`
+--
+CREATE DATABASE IF NOT EXISTS `zf2tutorial` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `zf2tutorial`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+DROP TABLE IF EXISTS `album`;
+CREATE TABLE `album` (
+  `id` int(11) NOT NULL,
+  `artist` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `album`
+--
+
+INSERT INTO `album` (`id`, `artist`, `title`) VALUES
+(1, 'The  Military  Wives', 'In  My  Dreams'),
+(2, 'Adele', '21'),
+(3, 'Bruce  Springsteen', 'Wrecking Ball (Deluxe)'),
+(4, 'Lana  Del  Rey', 'Born  To  Die'),
+(5, 'Gotye', 'Making  Mirrors'),
+(6, 'Rajat', 'One'),
+(7, 'Gupta', 'Two');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `text`) VALUES
+(1, 'Blog #1', 'Welcome to my first blog post'),
+(2, 'Blog #2', 'Welcome to my second blog post'),
+(3, 'Blog #3', 'Welcome to my third blog post'),
+(4, 'Blog #4', 'Welcome to my fourth blog post'),
+(5, 'Blog #5', 'Welcome to my fifth blog post'),
+(6, 'Rajat', 'This is test if data is from DB');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
